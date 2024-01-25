3893221b17cfb3fee40edb73e5b3033cbd703490
zeno <157558834+7eNO@users.noreply.github.com>
2024-01-25 17:52:55 +0900

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,10 @@
+document.getElementById('updateButton').addEventListener('click', function() {
+  // 페이지 이동
+  window.location.href = 'https://playentry.org/';
+  
+  // 콘솔에 메시지 출력
+  console.log('안녕하세요');
+  
+  // Alert으로 메시지 출력
+  alert('안녕하세요');
+});
